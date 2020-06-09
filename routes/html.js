20fff08aacab562df6313996335c2f21040e0d39
Tro <noe.monsivais@gmail.com>
2020-06-09 09:47:35 -0500

    Added route to /stats page.

--- routes/html.js
+++ routes/html.js
@@ -5,6 +5,12 @@ router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+router.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+
+
 
 
 
