45a9b917aa99d3cd0c3a50032b084ca2c19e6f76
Tro <noe.monsivais@gmail.com>
2020-06-09 10:27:17 -0500

    ran seeder file, created api route to get all workouts.

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
 const workout = require("../models/workout");
 
-
-
+router.get("/api/workouts", function (req, res) {
+    workout.find({})
+        .then(workout => {
+            res.json(workout)
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 
 module.exports = router;
